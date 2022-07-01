5f0c32f8fc0e81fe8fe058dcaaacb2f8a06a9335
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2022-07-01 10:56:53 -0400

    remove startup validation [18]

--- src/SMAPI/Program.cs
+++ src/SMAPI/Program.cs
@@ -5,8 +5,6 @@ using System.Linq;
 using System.Reflection;
 using System.Threading;
 using StardewModdingAPI.Framework;
-using StardewModdingAPI.Toolkit.Serialization.Models;
-using StardewModdingAPI.Toolkit.Utilities;
 
 namespace StardewModdingAPI
 {
@@ -35,10 +33,6 @@ namespace StardewModdingAPI
             try
             {
                 AppDomain.CurrentDomain.AssemblyResolve += Program.CurrentDomain_AssemblyResolve;
-                Program.AssertGamePresent();
-                Program.AssertGameVersion();
-                Program.AssertSmapiVersions();
-                Program.AssertDepsJson();
                 Program.Start(args);
             }
             catch (BadImageFormatException ex) when (ex.FileName == EarlyConstants.GameAssemblyName)
@@ -99,77 +93,6 @@ namespace StardewModdingAPI
             }
         }
 
-        /// <summary>Assert that the game is available.</summary>
-        /// <remarks>This must be checked *before* any references to <see cref="Constants"/>, and this method should not reference <see cref="Constants"/> itself to avoid errors in Mono or when the game isn't present.</remarks>
-        private static void AssertGamePresent()
-        {
-            try
-            {
-                _ = Type.GetType($"StardewValley.Game1, {EarlyConstants.GameAssemblyName}", throwOnError: true);
-            }
-            catch (Exception ex)
-            {
-                // file doesn't exist
-                if (!File.Exists(Path.Combine(EarlyConstants.GamePath, $"{EarlyConstants.GameAssemblyName}.exe")))
-                    Program.PrintErrorAndExit("Oops! SMAPI can't find the game. Make sure you're running StardewModdingAPI.exe in your game folder.");
-
-                // can't load file
-                Program.PrintErrorAndExit(
-                    message: "Oops! SMAPI couldn't load the game executable. The technical details below may have more info.",
-                    technicalMessage: $"Technical details: {ex}"
-                );
-            }
-        }
-
-        /// <summary>Assert that the game version is within <see cref="Constants.MinimumGameVersion"/> and <see cref="Constants.MaximumGameVersion"/>.</summary>
-        private static void AssertGameVersion()
-        {
-            // min version
-            if (Constants.GameVersion.IsOlderThan(Constants.MinimumGameVersion))
-            {
-                ISemanticVersion? suggestedApiVersion = Constants.GetCompatibleApiVersion(Constants.GameVersion);
-                Program.PrintErrorAndExit(suggestedApiVersion != null
-                    ? $"Oops! You're running Stardew Valley {Constants.GameVersion}, but the oldest supported version is {Constants.MinimumGameVersion}. You can install SMAPI {suggestedApiVersion} instead to fix this error, or update your game to the latest version."
-                    : $"Oops! You're running Stardew Valley {Constants.GameVersion}, but the oldest supported version is {Constants.MinimumGameVersion}. Please update your game before using SMAPI."
-                );
-            }
-
-            // max version
-            if (Constants.MaximumGameVersion != null && Constants.GameVersion.IsNewerThan(Constants.MaximumGameVersion))
-                Program.PrintErrorAndExit($"Oops! You're running Stardew Valley {Constants.GameVersion}, but this version of SMAPI is only compatible up to Stardew Valley {Constants.MaximumGameVersion}. Please check for a newer version of SMAPI: https://smapi.io.");
-        }
-
-        /// <summary>Assert that the versions of all SMAPI components are correct.</summary>
-        /// <remarks>Players sometimes have mismatched versions (particularly when installed through Vortex), which can cause some very confusing bugs without this check.</remarks>
-        private static void AssertSmapiVersions()
-        {
-            // get SMAPI version without prerelease suffix (since we can't get that from the assembly versions)
-            ISemanticVersion smapiVersion = new SemanticVersion(Constants.ApiVersion.MajorVersion, Constants.ApiVersion.MinorVersion, Constants.ApiVersion.PatchVersion);
-
-            // compare with assembly versions
-            foreach (var type in new[] { typeof(IManifest), typeof(Manifest) })
-            {
-                AssemblyName assemblyName = type.Assembly.GetName();
-                ISemanticVersion assemblyVersion = new SemanticVersion(assemblyName.Version!);
-                if (!assemblyVersion.Equals(smapiVersion))
-                    Program.PrintErrorAndExit($"Oops! The 'smapi-internal/{assemblyName.Name}.dll' file is version {assemblyVersion} instead of the required {Constants.ApiVersion}. SMAPI doesn't seem to be installed correctly.");
-            }
-        }
-
-        /// <summary>Assert that SMAPI's <c>StardewModdingAPI.deps.json</c> matches <c>Stardew Valley.deps.json</c>, fixing it if necessary.</summary>
-        /// <remarks>This is needed to resolve native DLLs like libSkiaSharp.</remarks>
-        private static void AssertDepsJson()
-        {
-            string sourcePath = Path.Combine(Constants.GamePath, "Stardew Valley.deps.json");
-            string targetPath = Path.Combine(Constants.GamePath, "StardewModdingAPI.deps.json");
-
-            if (!File.Exists(targetPath) || FileUtilities.GetFileHash(sourcePath) != FileUtilities.GetFileHash(targetPath))
-            {
-                File.Copy(sourcePath, targetPath, overwrite: true);
-                Program.PrintErrorAndExit($"The '{Path.GetFileName(targetPath)}' file didn't match the game's version. SMAPI fixed it automatically, but you must restart SMAPI for the change to take effect.");
-            }
-        }
-
         /// <summary>Initialize SMAPI and launch the game.</summary>
         /// <param name="args">The command-line arguments.</param>
         /// <remarks>This method is separate from <see cref="Main"/> because that can't contain any references to assemblies loaded by <see cref="CurrentDomain_AssemblyResolve"/> (e.g. via <see cref="Constants"/>), or Mono will incorrectly show an assembly resolution error before assembly resolution is set up.</remarks>
@@ -180,61 +103,11 @@ namespace StardewModdingAPI
 
             // get mods path
             bool? developerMode = null;
-            string modsPath;
-            {
-                string? rawModsPath = null;
-
-                // get mods path from command line args
-                int pathIndex = Array.LastIndexOf(args, "--mods-path") + 1;
-                if (pathIndex >= 1 && args.Length >= pathIndex)
-                    rawModsPath = args[pathIndex];
-
-                // get developer mode from command line args
-                if (args.Contains("--developer-mode"))
-                    developerMode = true;
-                if (args.Contains("--developer-mode-off"))
-                    developerMode = false;
-
-                // get from environment variables
-                if (string.IsNullOrWhiteSpace(rawModsPath))
-                    rawModsPath = Environment.GetEnvironmentVariable("SMAPI_MODS_PATH");
-                if (developerMode is null)
-                {
-                    string? rawDeveloperMode = Environment.GetEnvironmentVariable("SMAPI_DEVELOPER_MODE");
-                    if (rawDeveloperMode != null)
-                        developerMode = bool.Parse(rawDeveloperMode);
-                }
-
-                // normalize
-                modsPath = !string.IsNullOrWhiteSpace(rawModsPath)
-                    ? Path.Combine(Constants.GamePath, rawModsPath)
-                    : Constants.DefaultModsPath;
-            }
+            string modsPath = Constants.DefaultModsPath;
 
             // load SMAPI
             using SCore core = new(modsPath, writeToConsole, developerMode);
             core.RunInteractively();
-        }
-
-        /// <summary>Write an error directly to the console and exit.</summary>
-        /// <param name="message">The error message to display.</param>
-        /// <param name="technicalMessage">An additional message to log with technical details.</param>
-        private static void PrintErrorAndExit(string message, string? technicalMessage = null)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(message);
-            Console.ResetColor();
-
-            if (technicalMessage != null)
-            {
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.WriteLine(technicalMessage);
-                Console.ResetColor();
-                Console.WriteLine();
-            }
-
-            Program.PressAnyKeyToExit(showMessage: true);
         }
 
         /// <summary>Show a 'press any key to exit' message, and exit when they press a key.</summary>
